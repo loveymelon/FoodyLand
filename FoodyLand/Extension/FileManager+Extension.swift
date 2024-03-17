fe010609b019c8a5d1ac890c9336e7c1cbd7a5ab
loveymelon <52749067+loveymelon@users.noreply.github.com>
2024-03-17 15:36:40 +0900

    RealmRepository typealias 구성, FileManager 구성

---
+++ FoodyLand/Extension/FileManager+Extension.swift
@@ -0,0 +1,86 @@
+//
+//  FileManager+Extension.swift
+//  FoodyLand
+//
+//  Created by 김진수 on 3/17/24.
+//
+
+import UIKit
+
+enum FileError: Error {
+    case saveError
+    case fileRemoveError
+    case fileNoExist
+    case dataError
+    case noDocument
+}
+
+typealias FileResult = Result<Void, FileError>
+
+extension UIViewController {
+    func saveImageToDocument(image: UIImage, fileName: String, imageName: String) -> FileResult {
+        // 마켓 고유 아이디로 파일명을 잡고 이미지 이름은 이미지 테이블에서 생기는 고유 아이도.jpg로 생성
+        guard let documentDirectory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first else { return .failure(.noDocument) }
+        
+        let directoryURL = documentDirectory.appendingPathComponent(fileName)
+        
+        let fileURL = directoryURL.appendingPathComponent("\(imageName).jpg")
+        
+        guard let data = image.jpegData(compressionQuality: 1) else { return .failure(.dataError) } // 용량을 낮추는 대신 데이터의 손실이 발생함 pngData가 비손실 그래픽임
+        
+        do {
+            try data.write(to: fileURL)
+        } catch {
+            return .failure(.saveError)
+        }
+        return .success(())
+    }
+    
+    func loadImageToDocument(imageName: String, fileName: String) -> Result<UIImage, FileError> {
+        
+        guard let documentDirectory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first else { return .failure(.noDocument) }
+        
+        let directoryURL = documentDirectory.appendingPathComponent(fileName)
+        
+        let fileURL = directoryURL.appendingPathComponent("\(imageName).jpg")
+        
+        if FileManager.default.fileExists(atPath: fileURL.path) {
+            
+            guard let image = UIImage(contentsOfFile: fileURL.path) else { return .failure(.dataError) }
+            
+            return .success(image)
+        } else {
+            return .success(UIImage.basic)
+        }
+        
+    }
+    
+    func removeImageFromDocument(imageName: String, fileName: String, noData: Bool) -> FileResult {
+        // viewModel에서 repository로 이미지 값이 있는지 없는지 체크 후 없다면 파일 자체를 삭제
+        
+        guard let documentDirectory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first else { return .failure(.noDocument) }
+        
+        let directoryURL = documentDirectory.appendingPathComponent(fileName)
+        
+        let fileURL = directoryURL.appendingPathComponent("\(imageName).jpg")
+        
+        if FileManager.default.fileExists(atPath: fileURL.path()) {
+            
+            do {
+                try FileManager.default.removeItem(at: fileURL)
+                
+                if noData {
+                    try FileManager.default.removeItem(at: directoryURL)
+                }
+                
+            } catch {
+                return .failure(.fileRemoveError)
+            }
+            
+        } else {
+            return .failure(.fileNoExist)
+        }
+        
+        return .success(())
+    }
+}
--- FoodyLand/RealmHelper/RealmRepository.swift
+++ FoodyLand/RealmHelper/RealmRepository.swift
@@ -15,10 +15,12 @@ enum RealmError: Error {
     case unknownError
 }
 
+typealias RealmResult = Result<Void, RealmError>
+
 class RealmRepository {
     private let realm = try! Realm()
     
-    func createItem<T: Object>(item: T) -> Result<Void, RealmError> {
+    func createItem<T: Object>(item: T) -> RealmResult {
         do {
             try realm.write {
                 realm.add(item)
@@ -31,13 +33,13 @@ class RealmRepository {
         }
     } // 카테고리를 만들 수도 있고 유저의 메모를 만들 수 있어서 제네릭으로 처리함, 이미지도 만들어야됨 순서는 카테고리 - 이미지 - 유저 메모 순으로 만들어야됨 유저메모를 만들때는 카테고리 유저 append를 시켜줘야된다.
     
-    func fetchItem<T: Object>(type: T.Type) -> Results<T> {
+    func fetchItem<T: Object>(type: T.Type) -> Result<Results<T>, RealmError> {
         let items = realm.objects(type.self) // 타입 자체가 필요하므로
         
-        return items
+        return .success(items)
     } // 카테고리를 가져올 수도 있고 유저의 메모들을 가져올 수도 있기 때문에
     
-    func updateItem(marketId: String, marketItem: UserDiary) -> Result<Void, RealmError> { // viewModel에서 값 비교후 다른게 있다면
+    func updateItem(marketId: String, marketItem: UserDiary) -> RealmResult { // viewModel에서 값 비교후 다른게 있다면
         let items = realm.objects(UserDiary.self) // 업데이트를 할때 몇 번째에 있는 데이터를 업데이트할 지 어떻게 알 수 있을까? 레코드들은 리스트형식으로 있는데 인덱스에 어떻게 접근할지 고민해보자.
         // 마켓의 고유 아이디로 접근할 수 있지 않을까? 이미지를 수정할때도 where문에서 마켓 고유 아이디랑 일치한 테이블을 가져와서 거기에 있는 이미지를 수정하는 것이다.
         
@@ -70,7 +72,7 @@ class RealmRepository {
         
     }
     
-    func deleteItem(marketId: String) -> Result<Void, RealmError> {
+    func deleteItem(marketId: String) -> RealmResult {
         
         let items = realm.objects(UserDiary.self)
         
@@ -90,7 +92,7 @@ class RealmRepository {
     } // 유저 테이블 삭제시 하위 테이블(카테고리, 사진)을 지우고 테이블을 지워야된다.
     // 삭제시에도 유저 사진 = 유저 메모 순으로 지워야된다.
     
-    func deleteCategory(text: String) -> Result<Void, RealmError> {
+    func deleteCategory(text: String) -> RealmResult {
         let items = realm.objects(Category.self)
         
         let item = items.where { item in
